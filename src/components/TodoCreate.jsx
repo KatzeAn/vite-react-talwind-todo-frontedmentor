3d240a4095f082d4e39623c9cf515131156b8555
Angela <angela.alfaro1033@gmail.com>
2025-02-02 11:49:49 -0500

    transition1

--- src/components/TodoCreate.jsx
+++ src/components/TodoCreate.jsx
@@ -22,7 +22,7 @@ const  TodoCreate = ({createTodo}) => {
         <input 
         type="text" 
         placeholder="create a new todo..."
-        className="w-full text-gray-400 outline-none dark:bg-gray-800"
+        className="w-full text-gray-400 outline-none dark:bg-gray-800 transition-all duration-500"
         value={title}
         onChange={(e) => setTitle(e.target.value)}
         />        
